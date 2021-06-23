99c455375e059165c47860e39fa47f50e77ea077
Felipe Gutierrez <1560@holbertonschool.com>
2021-06-22 21:29:46 -0500

    Add: addig task files

---
+++ 0x00-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,12 @@
+declare namespace Subjects {
+	export interface TeacherInterface {
+		firstName: string;
+		lastName: string;
+	}
+}  
+declare namespace Subjects {
+	export interface TeacherInterface {
+		firstName: string;
+		lastName: string;
+	}
+}
