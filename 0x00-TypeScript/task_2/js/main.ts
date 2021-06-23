ab1043aeca92997ffe674900a0afd666024fe123
Felipe Gutierrez <1560@holbertonschool.com>
2021-06-22 20:54:35 -0500

    Edit: classes

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,53 +1,41 @@
-interface DirectorsInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string; 
-  }
-  
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
-  }
-  
-  export class Director implements DirectorsInterface {
-    workFromHome(): string {
-      return 'Working from home';
-    }
-    getCoffeeBreak(): string {
-      return 'Getting a coffee break';
-    }
-    workDirectorTasks(): string {
-      return 'Getting to director tasks';
-    }
-  }
-  
-  export class Teacher implements TeacherInterface {
-    workFromHome(): string {
-      return 'Cannot work from home';
-    }
-    getCoffeeBreak(): string {
-      return 'Cannot have a break';
-    }
-    workTeacherTasks(): string {
-      return 'Getting to work';
-    }
+export interface DirectorsInterface {
+        workFromHome(): string;
+        getCoffeeBreak(): string;
+        workDirectorTasks(): string;
 }
-  
+
+export interface TeacherInterface {
+        workFromHome(): string;
+        getCoffeeBreak(): string;
+        workTeacherTasks(): string;
+}
+
+export class Director implements DirectorsInterface {
+	    workFromHome() { return 'Working from home'; }
+	    getCoffeeBreak() { return 'Getting a coffee break'; }
+	    workDirectorTasks() { return 'Getting to director tasks'; }
+}
+
+export class Teacher implements TeacherInterface {
+	    workFromHome() { return 'Cannot work from home'; }
+	    getCoffeeBreak() { return 'Cannot have a break'; }
+	    workTeacherTasks() { return 'Getting to work'; }
+}
+
 export function createEmployee(firstName: string, lastName: string, salary: number | string): Director | Teacher {
     if (salary as number && salary < 500) return new Teacher();
-    else return new Director(); 
+    else return new Director();
 }
-  
+
 export function isDirector(employee: Director | Teacher): employee is Director {
     return (employee instanceof Director) ? true : false;
 }
-  
+
 export function executeWork(employee: Director | Teacher): string {
     return isDirector(employee) ? employee.workDirectorTasks() : employee.workTeacherTasks();
 }
-  
+
 type Subjects = 'Math' | 'History';
-export function teachClass(todayClass: Subjects): string{
+export function teachClass(todayClass: Subjects): string {
     return todayClass === 'Math' ? 'Teaching Math' : 'Teaching History';
 }
